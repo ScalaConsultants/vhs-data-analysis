5e87cd2496eba5c6fdeb463c8b230ddaedd6c30f
lucasrpb <lucasrpb@gmail.com>
2021-12-23 09:16:17 -0300

    Spark queries to plot visualizations.

--- analyze-vhs-data/src/main/scala/Queries.scala
+++ analyze-vhs-data/src/main/scala/Queries.scala
@@ -3,22 +3,13 @@ import org.apache.spark.sql.functions._
 
 object Queries {
 
-  def getPartOfDay(partOfDay: Int): String = {
-    partOfDay match {
-      case 0 => "morning"
-      case 1 => "afternoon"
-      case 2 => "evening"
-      case 3 => "night"
-    }
-  }
-
-  def getOrganic(flag: Int): String = {
+  /*def getOrganic(flag: Int): String = {
     flag match {
       case 0 => "not organic"
       case 1 => "organic"
     }
   }
-
+*/
   def playingsByCluster(df: DataFrame): Seq[Tuple3[String, String, Long]] = {
     df.groupBy("cluster", "partOfDay").agg(countDistinct("userId" ) as "count").collect().map { r =>
       val i0 = r.fieldIndex("cluster")
@@ -27,7 +18,7 @@ object Queries {
 
       val cluster = r.getInt(i0).toString
 
-      val partOfDay = getPartOfDay(r.getInt(i1))
+      val partOfDay = r.getString(i1)
       val count = r.getLong(i2)
 
       Tuple3(cluster, partOfDay, count)
@@ -42,7 +33,7 @@ object Queries {
 
       val cluster = r.getInt(i0).toString
 
-      val partOfDay = getPartOfDay(r.getInt(i1))
+      val partOfDay = r.getString(i1)
       val count = r.getLong(i2)
 
       Tuple3(cluster, partOfDay, count)
@@ -57,7 +48,7 @@ object Queries {
 
       val cluster = r.getInt(i0).toString
 
-      val partOfDay = getPartOfDay(r.getInt(i1))
+      val partOfDay = r.getString(i1)
       val count = r.getLong(i2)
 
       Tuple3(cluster, partOfDay, count)
@@ -72,26 +63,11 @@ object Queries {
 
       val cluster = r.getInt(i0).toString
 
-      val organic = r.getInt(i1)
+      val organic = r.getString(i1)
       val count = r.getLong(i2)
 
-      Tuple3(cluster, getOrganic(organic), count)
+      Tuple3(cluster, organic, count)
     }
   }
-
-  /*def genericSum[T](df: DataFrame, groupingColumns: Seq[String], sumColumn: String): Seq[Tuple3[String, String, T]] = {
-    df.groupBy(groupingColumns.map(col(_)): _*).sum(sumColumn).collect().map { r =>
-      val i0 = r.fieldIndex(groupingColumns(0))
-      val i1 = r.fieldIndex(groupingColumns(1))
-      val i2 = r.fieldIndex(s"sum($sumColumn)")
-
-      val cluster = r.getInt(i0).toString
-
-      val partOfDay = getPartOfDay(r.getInt(i1))
-      val count = r.getAs[T](i2)
-
-      Tuple3(cluster, partOfDay, count)
-    }
-  }*/
 
 }
